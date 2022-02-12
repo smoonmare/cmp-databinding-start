02f085a5180bed23f9066b8080567d02c2d37086
smoonmare <smoonmare@gmail.com>
2022-02-11 17:07:48 -0800

    Created interface. Updated the app to run on TS 4.4 and Angular 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Server } from './server.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databinding-start';
+  serverElements: Server[] = [
+    {
+      type: 'test server',
+      name: 'Test server 1',
+      content: 'Test content'
+    }
+  ];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
